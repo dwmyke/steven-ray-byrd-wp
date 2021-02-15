9dd354a45642c17f77922841c1cee7e679574134
Mikel-Jon West <dwmyke@gmail.com>
2021-02-14 18:29:35 -0800

    Attempted adding fonts to chakra-ui theme. Didm't help.

--- src/@chakra-ui/gatsby-plugin/theme.js
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -1,7 +1,10 @@
 import { extendTheme } from "@chakra-ui/react"
 
 const theme = extendTheme({
-    
+    fonts: {
+        heading: "Montserrat",
+        body: "Merriweather",
+      },
 })
 
 export default theme
